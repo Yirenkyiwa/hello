a6d9f298fe772c2d4131350c389e5b0c6d210340
marian yirenkyiwa larbi <marianlarbi2016@gmail.com>
2019-07-19 09:25:31 +0000

    python

--- myapp.py
+++ myapp.py
@@ -1,34 +1,59 @@
 from flask import *
 import json
 import os
+import sqlite3
+
 app = Flask(__name__)
 items = []
-@app.route('/foo/<name>')
-def foo(name):
-    return render_template('index.html',to=name)
+
+def create_table():
+    print("Creating table...")
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS todos (item text);")
+    conn.commit()
+
+def get_todos_from_db():
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    c.execute("SELECT item FROM todos;")
+    return list(map(lambda row: row[0], c.fetchall()))
+
+def add_todo_to_db(item):
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO todos VALUES ('" + item + "');")
+    conn.commit()
 
 @app.route('/')
 def index():
+    items=get_todos_from_db()
     return render_template('index.html',items=items)
 
 
 @app.route('/add_todo')
 def add_todo():
     item=request.args.get("item")
-    dd=request.args.get("date")
-    answer= item + "<br>" + dd
-    items.append(item)
-    return answer
-    return redirect("http://localhost:5000/", code=302)
+    #dd=request.args.get("date")
+    #answer= item + "<br>" + dd
+    #items.append(item)
+    print(item)
+    #return answer
+    return redirect("/", code=302)
 
 @app.route('/get_todos')
 def get_todos():
+    items = get_todos_from_db()
     resp=Response(json.dumps(getlist))
     resp.header['Content-Type']='application/json'
     return resp
 
- 
+@app.route('/foo/<name>')
+def foo(name):
+    return render_template('index.html',to=name)
+
 if __name__ == '__main__':
+    create_table()
     port = os.environ.get('PORT', 5000)
     app.run(debug=True, host='0.0.0.0',port=port)
     
